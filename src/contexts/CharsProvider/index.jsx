10726d86cb8eaae2d6d24bac12836ce010d4a374
ASOCezar <aso.cezar00@gmail.com>
2021-07-26 23:03:38 -0300

    responsivity work

--- src/contexts/CharsProvider/index.jsx
+++ src/contexts/CharsProvider/index.jsx
@@ -12,7 +12,7 @@ export const CharsProvider = ({ children }) => {
 
   useEffect(() => {
     fetch(
-      `https://gateway.marvel.com/v1/public/characters?ts=${timestamps}&apikey=${publicKey}&hash=${md5}&limit=${limit}`,
+      `http://gateway.marvel.com/v1/public/characters?ts=${timestamps}&apikey=${publicKey}&hash=${md5}&limit=${limit}`,
       {
         method: 'GET',
         headers: {
